82ddd61b130cf95a34732f44374609a750be64bd
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2014-09-10 17:37:20 -0300

    Project creation, cleaning from Networking project

---
+++ search/Search/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Search
+//
+//  Created by Mauricio Minestrelli on 9/10/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+    }
+}
